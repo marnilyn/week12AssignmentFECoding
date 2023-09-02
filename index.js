1efb4db820eca8a18ca369e41cf3518ae6fe8b70
Marnilyn Ramirez <You@example.com>
2023-09-01 23:36:03 -0400

    edits to html and js

--- index.js
+++ index.js
@@ -1,25 +1,65 @@
-const URL_ENDPOINT = 'https://64f15eda0e1e60602d23b9f2.mockapi.io/week12AssignmentFECoding_API/:endpoint'
 
-$.get(URL_ENDPOINT).then(data => {
+// Coding Steps:
+
+  // Create a full CRUD application of your choice using an API or JSON Server.
+  // Use JQuery/AJAX to interact with the API. 
+  // Use a form to post new entities.
+  // Build a way for users to update or delete entities.
+  // Include a way to get entities from the API.
+  // Use Bootstrap and CSS to style your project.
+
+
+const URL_ENDPOINT = 'https://64f15eda0e1e60602d23b9f2.mockapi.io/week12AssignmentFECoding_API/books'
+
+function getDataRenderTable() {
+  $.get(URL_ENDPOINT).then(data => {
+    $('tbody').empty()
     data.map(person => {
       $('tbody').append(
         $(`
         <tr>
-          <td>${person.id}</td>
-          <td>${person.full-Name}</td>
-          <td>${person.book-Name}</td>
-        </tr>
+        <td>${person.id}</td>
+          <td>${person.fullName}</td>
+          <td>${person.bookName}</td>
+          <td><button onclick="deleteUser(${person.id})">🗑</button></td>
+          </tr>
         `)
       )
     })
   })
+}
 
-  // <td><button onclick="deleteUser(${person.id})">🗑</button></td>
+getDataRenderTable()
 
-
-  $('#myBtn').click(function () {
+ 
+  $('#myBtn').click(function (e) {
+      e.preventDefault()
       $.post(URL_ENDPOINT, {
-        fullName: $('#full-Name').val(),
-        bookName:$('#book-Name').val(),
-      })
+        fullName: $('#fullName').val(),
+        bookName:$('#bookName').val(),
+      }).then(getDataRenderTable)
     })
+
+
+  function deleteUser(id) {
+    $.ajax(`${URL_ENDPOINT}/${id}`, {
+    type: 'DELETE',
+      }).then(getDataRenderTable)
+    }
+
+
+    function updateUser() {
+      let id = $('#updateId').val()
+    
+      $.ajax(`${URL_ENDPOINT}/${id}`, {
+        method: 'PUT',
+        data: {
+          fullName: $('#updateName').val(),
+          bookName: $('#updateBook').val(),
+        }
+      })
+    }
+    
+    $('#updateInfo').click(updateUser)
+
+  
